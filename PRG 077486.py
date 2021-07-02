2c23df50750280154c7c4e9e48820daacf9ab369
Tay-Son <0stxxol@gmail.com>
2021-07-02 22:40:40 +0900

    210702

---
+++ PRG 077486.py
@@ -0,0 +1,8 @@
+def solution(lst_enroll, lst_referral, lst_seller, lst_amount):
+    answer = []
+    return answer
+
+print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
+               ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"],
+               ["young", "john", "tod", "emily", "mary"],
+               [12, 4, 2, 5, 10]))
