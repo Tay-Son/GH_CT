89d56f802e0afa56bd707e0b46a134eef0c267e6
Tay-Son <0stxxol@gmail.com>
2021-08-16 16:38:13 +0900

    210816

--- Codes-pp/BOJ 03679.py
+++ Codes-pp/BOJ 03679.py
@@ -7,23 +7,41 @@ def rad_(c_a, c_b):
     d_y = c_b[1] - c_a[1]
     return math.atan2(d_y, d_x)
 
+import sys
+from functools import cmp_to_key
+
+
+def cmp_(c_a, c_b):
+    temp_a = c_a[1] * c_b[2]
+    temp_b = c_a[2] * c_b[1]
+    return 1 if temp_a < temp_b else -1
+
 
 for _ in range(int(sys.stdin.readline())):
     lst_input = list(map(int, sys.stdin.readline().split()))
     lst_ = []
     for idx_ in range(lst_input[0]):
-        lst_.append((lst_input[2 * idx_], lst_input[2 * idx_ + 1]))
-    lst_c = list(range(len(lst_)))
-    lst_c.sort(key=lambda x: lst_[x][0])
-    lst_c.sort(key=lambda x: lst_[x][1])
-    lst_c = [lst_c[0]] + sorted(lst_c[1:], key=lambda x: rad_(lst_[lst_c[0]], lst_[x]))
+        lst_.append([idx_, lst_input[2 * idx_ + 1], lst_input[2 * idx_ + 2]])
+    lst_.sort(key=lambda x: x[1])
+    lst_.sort(key=lambda x: x[2])
+
+    print(lst_)
+
+    for idx_ in range(1, len(lst_)):
+        lst_[idx_][1] -= lst_[0][1]
+        lst_[idx_][2] -= lst_[0][2]
+    lst_[0][1] = 0
+    lst_[0][2] = 0
+    lst_ = [lst_[0]] + sorted(lst_[1:], key=cmp_to_key(cmp_))
+    print(lst_)
 
     ptr_ = -1
-    r_ = rad_(lst_[lst_c[0]], lst_[lst_c[ptr_]])
-    while rad_(lst_[lst_c[0]], lst_[lst_c[ptr_ - 1]]) == r_:
+    while lst_[ptr_ - 1][1] * lst_[-1][2] == lst_[ptr_ - 1][2] * lst_[-1][1] and ptr_ > -len(lst_) + 1:
         ptr_ -= 1
-    lst_c = lst_c[:ptr_] + sorted(lst_c[ptr_:], key=lambda x: lst_[x][1], reverse=True)
 
-    print(' '.join(map(str, lst_c)))
+    print(lst_)
+    lst_[ptr_:].sort(key=lambda x: x[1] * x[1] + x[2] * x[2], reverse=True)
 
+    print(' '.join(map(lambda x: str(x[0]), lst_)))
+    print(lst_, ptr_)
 exit()
