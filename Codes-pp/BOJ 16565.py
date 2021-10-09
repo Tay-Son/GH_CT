9cd1b0e008e6cf273ead602c450a900d3c99eb1a
Tay-Son <0stxxol@gmail.com>
2021-10-09 13:45:29 +0900

    211009

--- Codes-pp/BOJ 16565.py
+++ Codes-pp/BOJ 16565.py
@@ -1,26 +1,28 @@
 import sys
-from itertools import combinations as cb
 
-dct_cb = dict()
+DIV_ = 10007
+
+dct_comb = {}
 
 
-def comb_(N_, C_):
-    if (N_, C_) not in dct_cb:
-        ret_ = 1
-        for a_, b_ in zip(range(N_, N_ - C_ - 1, -1), range(1, C_ + 1)):
-            ret_ *= a_
-            ret_ //= b_
-        dct_cb[(N_, C_)] = ret_
-    return dct_cb[(N_, C_)]
+def comb_(val_a, val_b):
+    val_b = min(val_b, val_a - val_b)
+    if (val_a, val_b) not in dct_comb:
+        if not val_b:
+            dct_comb[(val_a, val_b)] = 1
+        else:
+            dct_comb[val_a, val_b] = comb_(val_a - 1, val_b - 1) + comb_(val_a - 1, val_b)
+    return dct_comb[val_a, val_b]
 
 
 N_ = int(sys.stdin.readline())
+tot_ = 0
+for num_ in range(4, N_ + 1, 4):
+    if (num_ // 4) % 2:
+        tot_ += comb_(13, num_ // 4) * comb_(52 - num_, N_ - num_)
+    else:
+        tot_ -= comb_(13, num_ // 4) * comb_(52 - num_, N_ - num_)
+    tot_ %= DIV_
 
-if N_ < 4:
-    print(0)
-else:
-    tot_ = 13
-
-
-    print(tot_)
+print(tot_)
 exit()
