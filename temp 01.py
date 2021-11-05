dc5ad82daf297ee5e67937cff77b0705e23490b5
Tay-Son <0stxxol@gmail.com>
2021-11-05 10:22:37 +0900

    211105

--- temp 01.py
+++ temp 01.py
@@ -1,7 +1,4 @@
-def solution(lst_number):
-    set_ = set(lst_number)
-    tot_ = 0
-    for num_ in range(1, 10):
-        if num_ not in set_:
-            tot_ += num_
-    return tot_
+def solution():
+    return
+
+print(solution())
